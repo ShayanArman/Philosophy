60d55123a5aca3402e8358503303613c592ff870
Shayan Arman <shayan.arman2@gmail.com>
2015-01-08 14:39:36 +0100

    Added comments explaining solution

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -18,8 +18,8 @@ def findPhilosophyPath(start_page):
 			break
 		
 		try:
+			# Create a URL: 'http://en.wikipedia.org' + page_name
 			link = WIKI_URL_PREFIX + link_suffix
-			# print link
 			html_content = urlLib.urlopen(link).read()
 		except urlLib.URLError as e:
 			print ("Broken link")
@@ -28,13 +28,23 @@ def findPhilosophyPath(start_page):
 			print "Timed out trying to connect to link"
 
 		b_soup = BeautifulSoup(html_content)
+		
+		# Main content block of the Wiki entry
 		content_block = b_soup.find(id="mw-content-text")
+		
+		# Take all non-nested paragraphs and unordered lists
 		paragraphs_lists = content_block.find_all('p', recursive=False) + content_block.find_all('ul', recursive=False)
+		
+		# First create a list of all the links contained in each paragraph and list
+		# Reduce this list of lists using the reduceLinksList method into one List
+		# containing all the links
 		links_array = reduce( reduceLinksList, map(findLinks, paragraphs_lists))
 
 
 		for l in links_array:
 			link_suffix = str(l.get('href'))
+			# Continue searching through links until a link that has not been followed
+			# before and is a main wikipedia article is found
 			if(link_suffix not in path_array 
 				and '/wiki/' in link_suffix 
 				and ':' not in link_suffix 
@@ -59,6 +69,7 @@ def printPhilosophyPath(start_page):
 	else:
 		path_array = findPhilosophyPath(start_page_suffix)
 		path_length = len(path_array)
+		
 		if(path_length > 1 and path_array[-1] == TARGET_SUFFIX):
 			for suffix in path_array:
 				print WIKI_URL_PREFIX + suffix
